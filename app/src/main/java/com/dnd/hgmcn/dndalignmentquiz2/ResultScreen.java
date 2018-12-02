b95124a9dc21e646e3b570750e58c43312b1746e
hgmcn <hgm4@illinois.edu>
2018-12-02 17:09:35 -0600

    Initial commit

---
+++ app/src/main/java/com/dnd/hgmcn/dndalignmentquiz2/ResultScreen.java
@@ -0,0 +1,13 @@
+package com.dnd.hgmcn.dndalignmentquiz2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_screen);
+    }
+}
---
+++ app/src/main/java/com/dnd/hgmcn/dndalignmentquiz2/StartScreen.java
@@ -0,0 +1,17 @@
+package com.dnd.hgmcn.dndalignmentquiz2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    //android:background="@drawable/oldBook"
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
+
+
+
