bfdaa4fb959d12805d8ad825c39acef09649043d
hgmcn <hgm4@illinois.edu>
2018-12-03 18:35:17 -0600

    Kinda fucked it up, kinda not

---
+++ app/src/main/java/com/dnd/hgmcn/dndalignmentquiz2/NewStartScreen.java
@@ -0,0 +1,14 @@
+package com.dnd.hgmcn.dndalignmentquiz2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewStartScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_start_screen);
+    }
+
+}
--- app/src/main/java/com/dnd/hgmcn/dndalignmentquiz2/StartScreen.java
+++ app/src/main/java/com/dnd/hgmcn/dndalignmentquiz2/StartScreen.java
@@ -1,14 +1,16 @@
 package com.dnd.hgmcn.dndalignmentquiz2;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class StartScreen extends AppCompatActivity {
+public class StartScreen extends Activity {
 
-    //android:background="@drawable/oldBook"
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start_screen);
     }
 }
